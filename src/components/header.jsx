ad3def59de8d60391d2a8faf2b0778ecd96a839f
Milannn28 <milanpatelhave40@gmail.com>
2025-03-26 17:53:32 +0530

    update

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -101,7 +101,7 @@ const Header = () => {
                 <span>FAQ</span>
               </a>
             </div>
-            <a href="/dashboard" onClick={closeMobileMenu}>
+            <a href="/dashboard" onClick={closeMobileMenu} className="w-full md:w-fit">
               <button className="justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&amp;_svg]:pointer-events-none [&amp;_svg]:size-4 [&amp;_svg]:shrink-0 h-10 px-4 py-2 bg-purple-600 text-white hover:bg-purple-700 flex items-center space-x-2 w-full md:w-max">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-shield-check h-5 w-5">
                   <path d="M20 13c0 5-3.5 7.5-7.66 8.95a1 1 0 0 1-.67-.01C7.5 20.5 4 18 4 13V6a1 1 0 0 1 1-1c2 0 4.5-1.2 6.24-2.72a1.17 1.17 0 0 1 1.52 0C14.51 3.81 17 5 19 5a1 1 0 0 1 1 1z"></path>
@@ -118,3 +118,4 @@ const Header = () => {
 };
 
 export default Header;
+
