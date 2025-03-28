2aa06a2605e946ef861eeb7c5e9cfc9fc141d7fb
Milannn28 <milanpatelhave40@gmail.com>
2025-03-28 12:06:29 +0530

    verification steps

--- src/components/wallet-verification.jsx
+++ src/components/wallet-verification.jsx
@@ -57,7 +57,7 @@ const WalletVerification = () => {
           <p className="sm:text-lg font-medium">Congratulations! Your wallet is secure!</p>
           <button 
             onClick={handleReset}
-            className="w-max bg-black text-white inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 bg-primary text-primary-foreground hover:bg-slate-500 h-10 px-4 py-2"
+            className="md:w-full w-max bg-black text-white inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 bg-primary text-primary-foreground hover:bg-slate-500 h-10 px-4 py-2"
           >
             Reset
           </button>
